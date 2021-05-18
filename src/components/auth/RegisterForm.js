1428a0fb332c631f55cf411d4a2590dcd5df310b
Dom Narongkorn <narongkorn1703@gmail.com>
2021-05-18 21:46:56 +0700

    edit ui

--- src/components/auth/RegisterForm.js
+++ src/components/auth/RegisterForm.js
@@ -11,19 +11,34 @@ import {
   FormControlLabel,
   Grid,
 } from "@material-ui/core";
-
-import React, { useState } from "react";
+import axios from "../../config/axios";
+import React, { useContext, useState } from "react";
 import { useStyle } from "../layout/UseStyleLogin";
 import { MdEmail } from "react-icons/md";
 import { Visibility, VisibilityOff } from "@material-ui/icons";
+import { useHistory } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContextProvider";
+import { setToken } from "../../services/localStorageService";
 function RegisterForm({ getModalStyle }) {
   const classes = useStyle();
+  const history = useHistory();
+  const { setIsAuthenticated } = useContext(AuthContext);
+
   const [modalStyle] = useState(getModalStyle);
   const [typePassword, setTypePassword] = useState(false);
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState({
+    email: "",
+    firstName: "",
+    lastName: "",
+    password: "",
+    confirmPassword: "",
+    gender: "",
+    birthDate: "",
+  });
 
-  const handleChange = (event) => {
-    setValue(event.target.value);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValue((prev) => ({ ...prev, [name]: value }));
   };
   const handleClickShowPassword = () => {
     if (typePassword === true) {
@@ -32,6 +47,38 @@ function RegisterForm({ getModalStyle }) {
       setTypePassword(true);
     }
   };
+  const handleSignUp = async () => {
+    const {
+      email,
+      firstName,
+      lastName,
+      password,
+      confirmPassword,
+      gender,
+      birthDate,
+    } = value;
+    try {
+      const res = await axios.post("/register", {
+        email,
+        firstName,
+        lastName,
+        password,
+        confirmPassword,
+        gender,
+        birthDate,
+      });
+      if (res) {
+        console.log(res);
+        setIsAuthenticated(true);
+        setToken(res.data.token);
+        history.push("/homepage");
+      }
+    } catch (err) {
+      console.log(err.response);
+      console.dir(err.response.message);
+    }
+  };
+  console.log(value);
   return (
     <>
       <div style={modalStyle} className={classes.paper}>
@@ -41,15 +88,18 @@ function RegisterForm({ getModalStyle }) {
           style={{
             display: "flex",
             flexDirection: "column",
-          }}
-        >
+          }}>
           <span>
             <TextField
               placeholder="Firstname"
               variant="outlined"
+              name="firstName"
+              onChange={handleChange}
               className={classes.inputBase}
             />
             <TextField
+              name="lastName"
+              onChange={handleChange}
               placeholder="Lastname"
               variant="outlined"
               className={classes.inputBase}
@@ -58,6 +108,8 @@ function RegisterForm({ getModalStyle }) {
           <TextField
             placeholder="Email Address"
             variant="outlined"
+            onChange={handleChange}
+            name="email"
             type="email"
             className={classes.inputEmail}
             InputProps={{
@@ -72,7 +124,9 @@ function RegisterForm({ getModalStyle }) {
           <TextField
             id="outlined-adornment-password"
             variant="outlined"
+            name="password"
             placeholder="Password"
+            onChange={handleChange}
             type={typePassword ? "text" : "password"}
             className={classes.inputEmail}
             InputProps={{
@@ -84,9 +138,12 @@ function RegisterForm({ getModalStyle }) {
                       position: "absolute",
                       right: "5px",
                     }}
-                    onClick={handleClickShowPassword}
-                  >
-                    {typePassword ? <Visibility /> : <VisibilityOff />}
+                    onClick={handleClickShowPassword}>
+                    {typePassword ? (
+                      <Visibility />
+                    ) : (
+                      <VisibilityOff />
+                    )}
                   </IconButton>
                 </InputAdornment>
               ),
@@ -96,6 +153,8 @@ function RegisterForm({ getModalStyle }) {
             id="outlined-adornment-password"
             variant="outlined"
             placeholder="ConfirmPassword"
+            name="confirmPassword"
+            onChange={handleChange}
             type={typePassword ? "text" : "password"}
             className={classes.inputEmail}
             InputProps={{
@@ -107,9 +166,12 @@ function RegisterForm({ getModalStyle }) {
                       position: "absolute",
                       right: "5px",
                     }}
-                    onClick={handleClickShowPassword}
-                  >
-                    {typePassword ? <Visibility /> : <VisibilityOff />}
+                    onClick={handleClickShowPassword}>
+                    {typePassword ? (
+                      <Visibility />
+                    ) : (
+                      <VisibilityOff />
+                    )}
                   </IconButton>
                 </InputAdornment>
               ),
@@ -119,8 +181,10 @@ function RegisterForm({ getModalStyle }) {
             <Grid item xs={7}>
               <TextField
                 id="date"
+                onChange={handleChange}
                 label="Date of birth"
                 type="date"
+                name="birthDate"
                 variant="outlined"
                 className={classes.inputBirthdate}
                 InputLabelProps={{
@@ -130,13 +194,14 @@ function RegisterForm({ getModalStyle }) {
             </Grid>
             <Grid item xs={5}>
               <FormControl component="fieldset">
-                <FormLabel className={classes.labelGender}>Gender</FormLabel>
+                <FormLabel className={classes.labelGender}>
+                  Gender
+                </FormLabel>
                 <RadioGroup
                   aria-label="gender"
-                  name="gender1"
-                  value={value}
-                  onChange={handleChange}
-                >
+                  name="gender"
+                  value={value.gender}
+                  onChange={handleChange}>
                   <FormControlLabel
                     value="FEMALE"
                     control={<Radio />}
@@ -158,8 +223,8 @@ function RegisterForm({ getModalStyle }) {
           </Grid>
           <Button
             style={{ marginLeft: "160px" }}
-            className={classes.buttonRegister}
-          >
+            onClick={handleSignUp}
+            className={classes.buttonRegister}>
             Sign Up
           </Button>
         </form>
